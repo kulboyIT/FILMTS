9e1ad39b3cb9dd2ae6e48a20fc88001cb68f12f9
kulboyIT <86550475+kulboyIT@users.noreply.github.com>
2024-03-29 13:12:56 +0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -23,11 +23,11 @@ const Footer = () => {
                 d='M12 2.04c-5.5 0-10 4.49-10 10.02c0 5 3.66 9.15 8.44 9.9v-7H7.9v-2.9h2.54V9.85c0-2.51 1.49-3.89 3.78-3.89c1.09 0 2.23.19 2.23.19v2.47h-1.26c-1.24 0-1.63.77-1.63 1.56v1.88h2.78l-.45 2.9h-2.33v7a10 10 0 0 0 8.44-9.9c0-5.53-4.5-10.02-10-10.02Z'
               />
             </svg>
-         
+           </a>
          
         </div>
         <p className='mt-8 text-2xl leading-6 text-center text-gray-500 dark:text-gray-300'>
-          © 2023 KulFlix™. KUL ERO.
+          © 2024 KulFlix™. KUL ERO.
         </p>
       </div>
     </footer>
