0815b6af32a3a5c4657e85b4540955ddf1a23afd
kulboyIT <86550475+kulboyIT@users.noreply.github.com>
2024-04-26 15:41:20 +0700

    Update path.ts

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -1,19 +1,19 @@
 const PATH = {
   home: '/',
-  list: '/danh-sach',
+  list: 'danh-sach',
   watch: '/xem-phim',
   type: ':type',
   slug: ':slug',
-  search: '/tim-kiem',
+  search: 'tim-kiem',
   new: 'phim-moi',
   series: 'phim-bo',
   odd: 'phim-le',
   tvShows: 'tv-shows',
   anime: 'hoat-hinh',
-  genres: '/the-loai',
-  country: '/quoc-gia',
-  hot: '/hot',
-  film: '/phim'
+  genres: 'the-loai',
+  country: 'quoc-gia',
+  hot: 'hot',
+  film: 'phim'
 } as const
 
 export default PATH
