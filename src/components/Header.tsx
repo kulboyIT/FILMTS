da300feb5c1505cae147b4c7b734fdcec69cbed4
kulboyIT <86550475+kulboyIT@users.noreply.github.com>
2024-05-01 17:25:40 +0700

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -262,23 +262,93 @@ const Header = () => {
             className='text-gray-300 hover:text-gray-100'
           >
             <span className='sr-only'>VPN Google Play</span>
-           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-      <path
-        fill="#2196F3"
-        d="M8.32 7.68L.58 15.42c-.37-.35-.57-.83-.57-1.35V1.93C.01 1.4.22.92.6.56l7.72 7.12z"
-      ></path>
-      <path
-        fill="#FFC107"
-        d="M15.01 8c0 .7-.38 1.32-1.01 1.67l-2.2 1.22-2.73-2.52-.75-.69 2.89-2.89L14 6.33c.63.35 1.01.97 1.01 1.67z"
-      ></path>
-      <path
-        fill="#4CAF50"
-        d="M8.32 7.68L.6.56C.7.46.83.37.96.29 1.59-.09 2.35-.1 3 .26l8.21 4.53-2.89 2.89z"
-      ></path>
-      <path
-        fill="#F44336"
-        d="M11.8 10.89L3 15.74c-.31.18-.66.26-1 .26-.36 0-.72-.09-1.04-.29a1.82 1.82 0 01-.38-.29l7.74-7.74.75.69 2.73 2.52z"
-      ></path>
+           <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="80"
+      height="80"
+      fill="none"
+      viewBox="0 0 32 32"
+    >
+      <g>
+        <mask
+          id="mask0_87_8320"
+          style={{ maskType: "alpha" }}
+          width="24"
+          height="26"
+          x="7"
+          y="3"
+          maskUnits="userSpaceOnUse"
+        >
+          <path
+            fill="#C4C4C4"
+            d="M30.048 14.4a1.82 1.82 0 010 3.2L9.756 28.766c-1.235.68-2.756-.203-2.756-1.6V4.834c0-1.397 1.52-2.28 2.756-1.6L30.048 14.4z"
+          ></path>
+        </mask>
+        <g mask="url(#mask0_87_8320)">
+          <path
+            fill="url(#paint0_linear_87_8320)"
+            d="M7.635 28.547l12.657-12.73L7.843 3.3A1.807 1.807 0 007 4.834v22.332c0 .57.252 1.053.635 1.38z"
+          ></path>
+          <path
+            fill="url(#paint1_linear_87_8320)"
+            d="M30.048 14.4a1.82 1.82 0 010 3.2l-5.12 2.816-4.636-4.598 4.4-4.365 5.356 2.947z"
+          ></path>
+          <path
+            fill="url(#paint2_linear_87_8320)"
+            d="M24.93 20.417l-4.638-4.6-12.657 12.73c.556.476 1.389.622 2.121.219l15.173-8.35z"
+          ></path>
+          <path
+            fill="url(#paint3_linear_87_8320)"
+            d="M7.843 3.299l12.449 12.519 4.4-4.365L9.756 3.234a1.859 1.859 0 00-1.913.065z"
+          ></path>
+        </g>
+        <defs>
+          <linearGradient
+            id="paint0_linear_87_8320"
+            x1="15.677"
+            x2="7.071"
+            y1="10.874"
+            y2="19.551"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#00C3FF"></stop>
+            <stop offset="1" stopColor="#1BE2FA"></stop>
+          </linearGradient>
+          <linearGradient
+            id="paint1_linear_87_8320"
+            x1="20.292"
+            x2="31.738"
+            y1="15.818"
+            y2="15.818"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#FFCE00"></stop>
+            <stop offset="1" stopColor="#FFEA00"></stop>
+          </linearGradient>
+          <linearGradient
+            id="paint2_linear_87_8320"
+            x1="7.369"
+            x2="22.595"
+            y1="30.1"
+            y2="17.894"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#DE2453"></stop>
+            <stop offset="1" stopColor="#FE3944"></stop>
+          </linearGradient>
+          <linearGradient
+            id="paint3_linear_87_8320"
+            x1="8.107"
+            x2="22.597"
+            y1="1.901"
+            y2="13.736"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#11D574"></stop>
+            <stop offset="1" stopColor="#01F176"></stop>
+          </linearGradient>
+        </defs>
+      </g>
     </svg>
           </a>
            <a
@@ -288,41 +358,42 @@ const Header = () => {
             className='text-gray-300 hover:text-gray-100'
           >
             <span className='sr-only'>VPN APP STORE</span>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120 120">
-      <defs>
-        <linearGradient
-          id="a"
-          x1="-1315.782"
-          x2="-1195.782"
-          y1="529.793"
-          y2="529.793"
-          gradientTransform="rotate(-90 -832.788 -362.994)"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop offset="0" stopColor="#1d6ff2"></stop>
-          <stop offset="1" stopColor="#1ac8fc"></stop>
-        </linearGradient>
-      </defs>
-      <path
-        fill="url(#a)"
-        fillRule="evenodd"
-        d="M120 26v68a25.948 25.948 0 01-26 26H26A25.948 25.948 0 010 94V26A25.948 25.948 0 0126 0h68a25.948 25.948 0 0126 26z"
-      ></path>
-      <path
-        fill="#fff"
-        fillRule="evenodd"
-        d="M82.6 69h14.9a5.5 5.5 0 010 11H82.6z"
-      ></path>
-      <path
-        fill="#fff"
-        fillRule="evenodd"
-        d="M64.3 69a7.853 7.853 0 017.9 7.9 8.149 8.149 0 01-.6 3.1H22.5a5.5 5.5 0 010-11zm-1.4-36.2v9.6h-6.4L48.7 29a5.197 5.197 0 119-5.2zm5.5 9.3l27.3 47.3a5.489 5.489 0 01-9.5 5.5L69.7 66.2c-1.5-2.8-2.6-5-3.3-6.2a15.039 15.039 0 012-17.9z"
-      ></path>
-      <path
-        fill="#fff"
-        fillRule="evenodd"
-        d="M46 74H33.3L62 24.3a5.489 5.489 0 019.5 5.5zm-6.7 11.5L34 94.8a5.489 5.489 0 11-9.5-5.5l3.9-6.8a8.598 8.598 0 013.9-.9 7.778 7.778 0 017 3.9z"
-      ></path>
+             <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="80"
+      height="80"
+      fill="none"
+      viewBox="0 0 32 32"
+    >
+      <g>
+        <circle
+          cx="16"
+          cy="16"
+          r="14"
+          fill="url(#paint0_linear_87_8317)"
+        ></circle>
+        <path
+          fill="#fff"
+          d="M18.447 8.654a1.106 1.106 0 00-.404-1.507 1.096 1.096 0 00-1.5.406l-.528.921-.527-.92a1.096 1.096 0 00-1.5-.407 1.106 1.106 0 00-.404 1.507l1.164 2.032-3.683 6.429H8.098A1.1 1.1 0 007 18.218a1.1 1.1 0 001.098 1.104h10.334c.091-.24.186-.65.085-1.027-.153-.567-.717-1.18-1.663-1.18H13.6l4.847-8.461zM11.636 20.542c-.187-.21-.607-.543-.975-.653-.561-.168-.987-.063-1.202.029l-.818 1.428a1.106 1.106 0 00.404 1.507 1.095 1.095 0 001.5-.406l1.091-1.905z"
+        ></path>
+        <path
+          fill="#fff"
+          d="M22.23 19.322h1.672A1.1 1.1 0 0025 18.218a1.1 1.1 0 00-1.098-1.103h-2.937l-3.308-5.774c-.245.235-.716.834-.788 1.514-.091.873.046 1.609.458 2.329 1.385 2.422 2.772 4.842 4.158 7.263.303.528.974.71 1.5.406.526-.304.707-.979.404-1.507l-1.16-2.024z"
+        ></path>
+        <defs>
+          <linearGradient
+            id="paint0_linear_87_8317"
+            x1="16"
+            x2="16"
+            y1="2"
+            y2="30"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#2AC9FA"></stop>
+            <stop offset="1" stopColor="#1F65EB"></stop>
+          </linearGradient>
+        </defs>
+      </g>
     </svg>
           </a>
       </div>
