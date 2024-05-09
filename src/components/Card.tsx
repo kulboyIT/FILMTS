cff2dfd051aa7412c9bf18bcd30ef81af1301b6e
kulboyIT <86550475+kulboyIT@users.noreply.github.com>
2024-05-09 19:12:49 +0700

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,7 +10,7 @@ const Card = ({ data }: { data?: items }) => {
           <img
             title={data.name}
             loading='eager'
-            src={`https://img.ophim16.cc/uploads/movies/${data.thumb_url}`}
+            src={`https://img.ophim.live/uploads/movies/${data.thumb_url}`}
             alt={data.name}
             className='h-[210px] sm:h-[384px] w-full object-cover mb-1'
           />
